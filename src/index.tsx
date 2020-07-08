790bfa6ef0fb652c01db37ca8b9b68aad2387139
Michael H <MichaelHahmigery@gmail.com>
2020-07-08 13:43:43 +0300

    Add react router dependency and add to application.

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 const store = createReduxStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
